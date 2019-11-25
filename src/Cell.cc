419c0da9fab242274a6b0246bf6909ecc1f10773
Naymul Hossain <33997227+n5hossai@users.noreply.github.com>
2019-11-25 16:30:05 -0500

    CELL class changed to struct
    -class changed to struct
    -commented out all the getters and setters
    -changed the default constructor to now directly changed the fiels in the cell
    - handled server ports and isEdge for the cells according to row and cols when creating the cell

--- src/Cell.cc
+++ src/Cell.cc
@@ -1,23 +1,125 @@
 #include "Cell.h"
+using namespace std;
 
-Cell::Cell(int row, int col){}
+Cell::Cell(int row, int col) : row(row), col(col)
+{
+    // text = '.';
+    if ((row == 0)  && ((col == 3) || (col == 4)))
+    {
+        this->text = 'S';
+        this->isServerPort = true;
+        this->whoseServerPort = 1; // for player 1
+        this->isEmpty = true; // it is always empty, because the links gets downloaded anyway.
 
-Cell::~Cell(){}
+        this->isFireWall = false;
+    }
+    else if ((row == 7)  && ((col == 3) || (col == 4)))
+    {
+        this->text = 'S';
+        this->isServerPort = true;
+        this->whoseServerPort = 2; // for player 2
+        this->isEmpty = true; // it is always empty, because the links gets downloaded anyway.
 
-int Cell::getRow(){}
+        this->isFireWall = false;
+    }
+    else
+    {
+        this->text = '.';
+        this->isServerPort = false;
+        this->whoseServerPort = 0; // NO ONE's serverport
+    }
 
-int Cell::getCol(){}
+    if ((row == 0) || (row == 7))
+    {
+        this->isEdge = true;
+    }
+}
 
-void Cell::setCellText(char text){}
+//getters:
+// int Cell::getRow() const
+// {
+//     return this->row;
+// }
+// int Cell::getCol() const
+// {
+//     return this->col;
+// }
+// char Cell::getText() const
+// {
+//     return this->text;
+// }
+// bool Cell::getIsServerPort() const
+// {
+//     return this->isServerPort;
+// }
+// int Cell::getWhoseServerPort() const
+// {
+//     return this->whoseServerPort;
+// }
+// bool Cell::getIsFireWall() const
+// {
+//     return this->isFireWall;
+// }
+// int Cell::getFireWallOwner() const
+// {
+//     return this->fireWallOwner;
+// }
+// bool Cell::getIsEmpty() const
+// {
+//     return this->isEmpty;
+// }
+// bool Cell::getIsEdge() const
+// {
+//     return this->isEdge;
+// }
 
-std::string Cell::getCellText(){}
+// //setters:
+// void Cell::setText(char text_)
+// {
+//     this->text = text_;
+// }
 
-bool Cell::isEdge(){}
+// void Cell::setIsServerPort(bool boolean_)
+// {
+//     this->isServerPort = boolean_;
+// }
+// void Cell::setWhoseServerPort(int whoseServerPort_)
+// {
+//     this->whoseServerPort = whoseServerPort_;
+// }
 
-int Cell::getEdgeOfPlayer(){}
+// void Cell::setRow(int row_)
+// {
+//     this->row = row_;
+// }
 
-bool Cell::getIsServerPort(){}
+// void Cell::setCol(int col_)
+// {
+//     this->col = col_;
+// }
 
-bool Cell::getHasFireWall(){}
+// void Cell::setText(char text_)
+// {
+//     this->text = text_;
+// }
 
-int Cell::getWhoBuiltFireWall(){}
+// void Cell::setIsServerPort(bool boolean_)
+// {
+//     this->isServerPort = boolean_;
+// }
+// void Cell::setIsFireWall(bool boolean_)
+// {
+//     this->isFireWall = boolean_;
+// }
+// void Cell::setFireWallOwner(int fireWallOwner_)
+// {
+//     this->fireWallOwner = fireWallOwner_;
+// }
+// void Cell::setIsEmpty(bool boolean_)
+// {
+//     this->isEmpty = boolean_;
+// }
+// void Cell::setIsEdge(bool boolean_)
+// {
+//     this->isEmpty = boolean_;
+// }
--- src/Cell.h
+++ src/Cell.h
@@ -1,28 +1,44 @@
 #ifndef CELL_H
 #define CELL_H
 #include <string>
+#include "Player.h"
+class Player;
+struct Cell
+{
 
-class Cell{
-    private:
-        int row;
-        int col;
-        char cellText;
-        bool isServerPort;
-        bool hasFireWall;
-        int whoBuiltFirewall; 
-        int edgeOfPlayer();
-    public:
-        Cell(int row, int col);
-        ~Cell();
-        int getRow();
-        int getCol();
-        void setCellText(char text);
-        std::string getCellText();
-        bool isEdge();
-        int getEdgeOfPlayer();
-        bool getIsServerPort();
-        bool getHasFireWall();
-        int getWhoBuiltFireWall();
+    int row;
+    int col;
+    char text;
+    bool isServerPort;
+    int whoseServerPort;
+    bool isFireWall;
+    int fireWallOwner;
+    bool isEmpty;
+    bool isEdge;
+
+
+
+    Cell(int row, int col);
+    //getters:
+    // int getRow() const;
+    // int getCol() const;
+    // char getText() const;
+    // bool getIsServerPort() const;
+    // int getWhoseServerPort() const;
+    // bool getIsFireWall() const;
+    // int getFireWallOwner() const;
+    // bool getIsEmpty() const;
+    // bool getIsEdge() const;
+    //setters:
+    // void setRow(int row_);
+    // void setCol(int col_);
+    // void setText(char text_);
+    // void setIsServerPort(bool boolean_);
+    // void setWhoseServerPort(int whoseServerPort_);
+    // void setIsFireWall(bool boolean_);
+    // void setFireWallOwner(int fireWallOwner_);
+    // void setIsEmpty(bool boolean_);
+    // void setIsEdge(bool boolean_);
 };
 
 #endif
