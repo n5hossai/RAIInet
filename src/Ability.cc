839344ad818a37dc48455eaf855888fe666f871c
Parv <parvathi.krishnan@hotmail.com>
2019-11-25 16:39:22 -0500

    added ability code

--- src/Ability.cc
+++ src/Ability.cc
@@ -1,11 +1,70 @@
 #include "Ability.h"
+#include <iostream>
 
-Ability::Ability(char code){}
+Ability::Ability(char code){
+    try{
+    switch (code)
+        {
+            case 'L':   
+                name = LinkBoost;
+                break;
+            case 'F':   
+                name = Firewall;
+                break;
+            case 'D':   
+                name = Download;
+                break;
+            case 'P':   
+                name = Polarize;
+                break;
+            case 'S':   
+                name = Scan;
+                break;
+            case 'N':   
+                name = Sand;
+                break;
+            case 'O':   
+                name = Portal;
+                break;
+            case 'R':   
+                name = Strengthen;
+                break;
+            default:    throw(code);
+        }
+    }catch(char e){
+        std::cout << "Invalid Ability";
+    }
+    isUsed = false;
+}
 
-Ability::~Ability(){}
+Ability::~Ability(){
 
-std::string Ability::getAbilityName(){}
+}
 
-bool Ability::getIsUsed(){}
+std::string Ability::getAbilityName(){
+    try{
+        switch (name)
+        {
+            case LinkBoost:   return "LinkBoost";
+            case Firewall:   return "Firewall";
+            case Download: return "Download";
+            case Polarize:   return "Polarize";
+            case Scan: return "Scan";
+            case Sand:   return "Sand";
+            case Portal: return "Portal";
+            case Strengthen: return "Strengthen";
+            default:      throw;
+        }
+    }
+    catch(char e) {
+        std::cout << "Invalid Ability";
+    }
+}
 
-void Ability::UseAbility(){}
+bool Ability::getIsUsed(){
+    return isUsed;
+}
+
+void Ability::UseAbility(){
+    isUsed = true;
+}
