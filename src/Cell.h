ad5cef85394ab1546fa9c628004eb40fb2ae040a
Parvathi Krishnan <parvathi.krishnan@hotmail.com>
2019-11-25 17:28:24 -0500

    Update Cell.h
    removing comments

--- src/Cell.h
+++ src/Cell.h
@@ -15,30 +15,7 @@ struct Cell
     int fireWallOwner;
     bool isEmpty;
     bool isEdge;
-
-
-
     Cell(int row, int col);
-    //getters:
-    // int getRow() const;
-    // int getCol() const;
-    // char getText() const;
-    // bool getIsServerPort() const;
-    // int getWhoseServerPort() const;
-    // bool getIsFireWall() const;
-    // int getFireWallOwner() const;
-    // bool getIsEmpty() const;
-    // bool getIsEdge() const;
-    //setters:
-    // void setRow(int row_);
-    // void setCol(int col_);
-    // void setText(char text_);
-    // void setIsServerPort(bool boolean_);
-    // void setWhoseServerPort(int whoseServerPort_);
-    // void setIsFireWall(bool boolean_);
-    // void setFireWallOwner(int fireWallOwner_);
-    // void setIsEmpty(bool boolean_);
-    // void setIsEdge(bool boolean_);
 };
 
 #endif
